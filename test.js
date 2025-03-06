1776e7f77e649052a40d875800413aea2665f39b
Niraj Dev <devniraj4@gmail.com>
2025-03-06 23:11:08 +0545

    first commit

---
+++ test.js
@@ -0,0 +1,33 @@
+
+
+// console.log(1+106) // print 
+// console.log("bye world")
+
+// node ko version 18.10 --> watch mode --> automatically rerun or restart
+
+// variable 
+
+// var/let/const variableName = value 
+// primitive datatype --> string , number, boolean
+// non primitive datatype --> array, object
+
+let cup = "chiya" // string
+let gamala = "dulo"  // string
+
+let bag = "book&copy"
+let test = "1bssswwss"
+
+const pi = 3.14  // number
+const gravity = 9.8 // number
+
+let isNepali = true // boolean
+let isIndian = false
+
+const password = "thisisencryptedpassword"
+
+console.log("Mero gamala ma chai yo cha? k xa ? " + gamala + " xa")
+console.log(`Mero gamala ma chai yo cha? k xa ? ${gamala} xa `)
+
+let age = 22 
+console.log("My age is " + age)
+console.log(`My age is ${age}`)
