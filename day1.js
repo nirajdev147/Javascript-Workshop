851d6fdd876293b5082484eb51d87ef5e6622e2c
Niraj Dev <devniraj4@gmail.com>
2025-03-20 19:25:21 +0545

    Added day1.js file

---
+++ day1.js
@@ -0,0 +1,51 @@
+//operators in javascript--> It is used to perform some operation on data.
+
+
+//Arithmetic Operators
+
+// let a = 5
+// let b = 2
+// console.log("a=",a, "b=",b);
+// a++;  b--;                       //post increment and decrement operator
+// ++a; --b;                        //pre increment and decrement operator
+// console.log("a=",a , "b=",b);
+// console.log("a + b =", a + b);  //addition operator
+// console.log("a - b =", a - b);  //subtraction operator
+// console.log("a * b =", a * b);  // multiplication operator
+// console.log("a / b =", a / b);  // division operator
+// console.log("a % b =", a % b);  // modulus operator
+// console.log("a ** b =", a ** b); // exponentiation operator
+
+
+
+//Assignment Operators
+
+// let a = 4;
+// let b = 2;
+// a += 4;      // a = a + 4
+// a -= 4;   // a = a - 4
+// a *= 4;   // a = a * 4
+// a /= 4;   // a = a / 4
+// a **= 4;  // a = a ** 4
+// a %= 4;  // a = a % 4
+// console.log("a =", a); 
+
+
+//Comparison Operators
+// let a = 4;
+// let b = 5;
+
+// //console.log("5 !== 5",a !== b);
+// console.log("4 <= 5", a <= b);
+
+
+//Logical Operators
+// let a = 7;
+// let b = 5;
+
+// // let cond1 = a > b;  //true
+// // let cond2 = a === 7;  //true
+// // console.log("cond1 && cond2", a > b && a === 7);     //logical  AND operator
+// // console.log("cond1 || cond2", a > b || a === 7);     //logical  OR operator
+// console.log("!(a > b)", !(a > b));                      //logical OR operator
+
