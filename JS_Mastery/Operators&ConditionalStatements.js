7fbbd5d42ff41a77c131f81e5df575a0406ac51d
Niraj Dev <devniraj4@gmail.com>
2025-04-21 22:22:40 +0545

    Deleted & Added new file

--- JS_Mastery/Operators&ConditionalStatements.js
+++ JS_Mastery/Operators&ConditionalStatements.js
@@ -1,3 +1,58 @@
+//operators in javascript--> It is used to perform some operation on data.
+
+
+//Arithmetic Operators
+
+// let a = 5;
+// let b = 2;
+// console.log("a=",a, "b=",b);
+// a++;  b--;                       //post increment and decrement operator
+// ++a; --b;                        //pre increment and decrement operator
+// console.log("a=",a , "b=",b);
+// console.log("a + b =", a + b);  //addition operator
+// console.log("a - b =", a - b);  //subtraction operator
+// console.log("a * b =", a * b);  // multiplication operator
+// console.log("a / b =", a / b);  // division operator
+// console.log("a % b =", a % b);  // modulus operator
+// console.log("a ** b =", a ** b); // exponentiation operator
+
+
+
+//Assignment Operators
+
+// let a = 4;
+// let b = 2;
+// a += 4;      // a = a + 4
+// a -= 4;   // a = a - 4
+// a *= 4;   // a = a * 4
+// a /= 4;   // a = a / 4
+// a **= 4;  // a = a ** 4
+// a %= 4;  // a = a % 4
+// console.log("a =", a); 
+
+
+//Comparison Operators
+// let a = 4;
+// let b = 5;
+
+// //console.log("5 !== 5",a !== b);
+// console.log("4 <= 5", a <= b);
+
+
+//Logical Operators
+// let a = 7;
+// let b = 5;
+
+// // let cond1 = a > b;  //true
+// // let cond2 = a === 7;  //true
+// // console.log("cond1 && cond2", a > b && a === 7);     //logical  AND operator
+// // console.log("cond1 || cond2", a > b || a === 7);     //logical  OR operator
+// console.log("!(a > b)", !(a > b));                      //logical OR operator
+
+
+
+
+
 // // // let name = "Niraj Dev";
 // // // let age = 24 ;
 // // // let address = "Biratnagar";
@@ -35,7 +90,7 @@
 
 
 
-/* ----------------------------------------conditional statements----------------------------------- */
+/* ----------------------------------------Start of conditional statements----------------------------------- */
 // let isRaining = true;
 
 // if(isRaining){
@@ -150,3 +205,6 @@
 // } else {
 //     console.log("Grade: F");
 // }
+
+
+/*------------------------End of conditional statements------------------------*/
